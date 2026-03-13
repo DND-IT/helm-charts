c791948460ed7e981d77cbf6f3990b1d10db9d0d
swibrow <15628653+swibrow@users.noreply.github.com>
2026-03-13 22:21:05 +0100

    fix(ci): install all AWS LBC v3 CRDs (base + gateway)

--- .github/install-crds.sh
+++ .github/install-crds.sh
@@ -14,9 +14,14 @@ kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/downloa
 # Experimental channel adds TCPRoute, TLSRoute, UDPRoute
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/${GATEWAY_API_VERSION}/experimental-install.yaml
 
-# AWS Load Balancer Controller v3 CRDs (TargetGroupConfiguration, LoadBalancerConfiguration, ListenerRuleConfiguration)
+# AWS Load Balancer Controller v3 CRDs
 # renovate: datasource=github-releases depName=aws-load-balancer-controller packageName=kubernetes-sigs/aws-load-balancer-controller
 AWS_LBC_VERSION="v3.1.0"
+# Base CRDs (TargetGroupBinding, IngressClassParams, ALBTargetControlConfig)
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${AWS_LBC_VERSION}/config/crd/bases/elbv2.k8s.aws_targetgroupbindings.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${AWS_LBC_VERSION}/config/crd/bases/elbv2.k8s.aws_ingressclassparams.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${AWS_LBC_VERSION}/config/crd/bases/elbv2.k8s.aws_albtargetcontrolconfigs.yaml
+# Gateway CRDs (TargetGroupConfiguration, LoadBalancerConfiguration, ListenerRuleConfiguration)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${AWS_LBC_VERSION}/config/crd/gateway/gateway.k8s.aws_targetgroupconfigurations.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${AWS_LBC_VERSION}/config/crd/gateway/gateway.k8s.aws_loadbalancerconfigurations.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${AWS_LBC_VERSION}/config/crd/gateway/gateway.k8s.aws_listenerruleconfigurations.yaml
